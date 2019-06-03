0507f74f2969d694ee21fede356f4d69f2717a37
duwns <duwns051004@naver.com>
2019-06-03 18:27:00 +0900

    어이없네

---
+++ greedy-lv1.cpp
@@ -0,0 +1,58 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int answer = 0;
+
+void change(string name, int index){
+	int a=name[index] - 'A';
+	int b='Z' - name[index] + 1;
+	if(a>b)	answer+=b;
+	else answer+=a;
+	//printf("%d ", answer);
+}
+
+
+int solution(string name) {
+   
+    int i, nota=0;
+	int index=0; //처음 시작은 무조건 0번째 인덱스 이다.  
+    
+    for(i=0; i<name.size(); i++)  // A가 아닌 문자 수 
+    	if(name[i]!='A') nota++;  
+    printf("%d", nota);
+    while(nota!=0){
+    	change(name, index);  //해당 인덱스의 문자를 'A'로 바꿔준다.  
+    	name[index]='A';
+    	nota--;
+    	if(nota == 0) break;
+    	
+    	int right=0, left=0;
+		int rindex=index;
+		int lindex=index;
+		
+    	while(name[rindex]=='A'){
+    		rindex++;	
+    		right++;
+    		if(rindex>=name.size()) rindex=0;
+		} 
+    	while(name[lindex]=='A'){
+    		lindex--;
+			left++;	
+    		if(lindex<0) lindex=name.size()-1;
+		} 
+    //	printf("%d %d\n", left, right);
+    	if(right > left){
+    		answer+=left;
+    		index=lindex;
+		} 
+    	else {
+    		answer+=right;
+    		index=rindex;
+		}
+    	
+	}
+    
+    return answer;
+}
